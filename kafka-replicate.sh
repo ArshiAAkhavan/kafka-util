887a035d58e505699dd4525bb0fdb5535eaabb94
ArshiA Akhavan <letmemakenewone@gmail.com>
2021-01-13 19:28:30 +0330

    error handling

--- kafka-replicate.sh
+++ kafka-replicate.sh
@@ -303,7 +303,7 @@ for partition_assignment in `$KAFKA_TOPICS_BIN --zookeeper $ZOOKEEPER_CONNECT --
   new_replicas=`scale $replicas $REPLICATION`
   if [ -z "$new_replicas" ]; then
       echo "ERROR: Cannot find any reassignment for partition $partition" >&2
-      new_partitions=$replicas
+      new_replicas=$replicas
   fi
   json="$json    {\"topic\": \"${TOPIC}\", \"partition\": ${partition}, \"replicas\": [${new_replicas}] },\n"
 done
