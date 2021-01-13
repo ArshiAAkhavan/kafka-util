5d1b2bbb09bf547e585c4b9b7465a48518e4ce16
ArshiA Akhavan <letmemakenewone@gmail.com>
2021-01-13 17:34:05 +0330

    typo

--- kafka-replicate.sh
+++ kafka-replicate.sh
@@ -393,7 +393,7 @@ function scale {
   
   local test_t=`echo $broker_range|tr ',' ' '`
   echo "test_t=$test_t" >&2
-  local new_partitions=(`echo $broker_range|tr ',' ' '`)
+  local new_partitions=($test_t)
   echo "new_partitions1=$new_partitions" >&2
   new_partitions=("${new_partitions[@]:0:$2}")
   echo "new_partitions2=$new_partitions" >&2
